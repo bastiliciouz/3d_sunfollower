91d9a5ea8bf0fa077a523d07a0a6ef27ac7fac51
Dariece <dmarlinghaus@gmail.com>
2021-05-15 10:24:53 +0200

    refactored database data field

--- database.py
+++ database.py
@@ -12,31 +12,31 @@ class DBConnect:
 
     def init_db_connection(self):
         try:
-            self.db_connection = pymysql.connect(host=f"{user.host}",
-                                                 user=f"{user.benutzer}",
-                                                 password=f"{user.passwd}",
-                                                 database=f"{user.db_connection}")
+            self.__db_connection = pymysql.connect(host=f"{user.host}",
+                                                   user=f"{user.benutzer}",
+                                                   password=f"{user.passwd}",
+                                                   database=f"{user.db_connection}")
             print("\n    +++Database connected+++")
         except Exception as e:
             print(e)
 
     def insert_aktuell(self, current_pos_unten, current_pos_oben):
         try:
-            with self.db_connection.cursor() as cursor:
+            with self.__db_connection.cursor() as cursor:
                 cursor.execute(f"""INSERT INTO
                                     current_pos(current_pos_unten, current_pos_oben)
                                     VALUES ({current_pos_unten},{current_pos_oben})""")
-                self.db_connection.commit()
+                self.__db_connection.commit()
 
         except Exception as e:
             print(e)
 
     def insert_error(self, message):
         try:
-            with self.db_connection.cursor() as cursor:
+            with self.__db_connection.cursor() as cursor:
                 cursor.execute(f"""INSERT INTO
                                     errors(message)
                                     VALUES ({message})""")
-                self.db_connection.commit()
+                self.__db_connection.commit()
         except:
             pass
